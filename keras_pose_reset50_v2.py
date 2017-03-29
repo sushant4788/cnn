748988593f13cce154aa585a933c8978313e97c0
Sushant Kulkarni <sushant4788@gmail.com>
2017-03-29 14:57:08 -0400

    aux loss

---
+++ keras_pose_reset50_v2.py
@@ -0,0 +1,56 @@
+from __future__ import print_function
+#from keras.preprocessing.image import ImageDataGenerator
+from imagenet_utils import preprocess_input, decode_predictions
+from keras.models import Model
+from keras.layers import Dense, Activation, Flatten
+from keras.layers import Dropout
+from keras.utils import np_utils
+from keras.callbacks import ReduceLROnPlateau, CSVLogger, EarlyStopping
+from keras.callbacks import TensorBoard
+from PIL import Image
+from resnet50 import ResNet50
+from keras.preprocessing import image
+from keras import optimizers
+import keras.backend as K
+import tensorflow as tf
+import numpy as np
+import gc
+import glob, os
+# import the functions from keras_pose_resnet.py
+import keras_pose_resnet
+# Parmas for training
+batch_size = 20
+num_epochs = 60
+
+img_rows, img_cols, img_channels = 224, 224, 3
+base_dir = '/home/sushant/Downloads/Kings/'
+
+def main():
+    # read the images
+    train_imgs, train_pose_tx, train_pose_rt, test_imgs, test_pose_tx, test_pose_rt = keras_pose_resnet.load_train_test_splits(base_dir )
+
+    base_model = ResNet50(weights ='imagenet')
+    # the output of the last layer of the resnet
+    y = base_model.get_layer('activation_5').output
+    y = Flatten()(y)
+    y = Dense(64, activation='tanh')(y)
+    int_position = Dense(3, activation='tanh')(y)
+    int_rotation = Dense(4, activation='tanh')(y)
+    x = base_model.output
+    x = Dense(64, activation='tanh')(x)
+    final_position = Dense(3, activation='tanh')(x)
+    final_rotation = Dense(4, activation='tanh')(x)
+    model = Model(input = base_model.input, output=[int_position, int_rotation,
+    final_position, final_rotation])
+
+    print(model.summary())
+    model.compile(optimizer='rmsprop', loss='mse', loss_weights = [0.5, 250.0, 1.0, 500.0])
+
+    model.fit(train_imgs, [train_pose_tx, train_pose_rt, train_pose_tx,
+    train_pose_rt], batch_size= batch_size, epochs = num_epochs, shuffle=True)
+
+    model.save('pose_resnet_v2.h5')
+    ts_pos_pred, ts_rot_pred = model.predict(test_imgs)
+
+if __name__ == '__main__':
+    main()
--- keras_pose_resnet.py
+++ keras_pose_resnet.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 #from keras.preprocessing.image import ImageDataGenerator
+from imagenet_utils import preprocess_input, decode_predictions
 from keras.models import Model
 from keras.layers import Dense, Activation, Flatten
 from keras.layers import Dropout
@@ -14,18 +15,122 @@ import keras.backend as K
 import tensorflow as tf
 import numpy as np
 import gc
+import glob, os
 
 # Set some hardcoded sizes
 img_rows ,img_cols, img_channels = 224, 224, 3
 # Some dir locations
-training_file = '/home/sushant/Downloads/Kings/KingsCollege/dataset_train_mod.txt'
-testing_file = '/home/sushant/Downloads/Kings/KingsCollege/dataset_test_mod.txt'
-dataset_location ='/home/sushant/Downloads/Kings/KingsCollege/'
+#training_file = '/home/sushant/Downloads/Kings/KingsCollege/dataset_train_mod.txt'
+#testing_file = '/home/sushant/Downloads/Kings/KingsCollege/dataset_test_mod.txt'
+#dataset_location ='/home/sushant/Downloads/Kings/KingsCollege/'
+base_dir ='/home/sushant/Downloads/Kings/'
 
-batch_size = 1
-num_epochs = 20
+batch_size = 20
+num_epochs = 2
 
-num_samples = 20
+#num_samples = 20
+def process_dataset(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+    # ============================
+    num_samples = len(lines)
+    #=============================
+    lines = [x.strip('\n') for x in lines]
+    pose = np.zeros((num_samples, 7), dtype='float32')
+    #print(pose.shape)
+    img_list = []
+    #for i in range(0, len(lines)):
+    for i in range(0, num_samples):
+        line_splits = lines[i].split(' ')
+        img_list.append(os.path.join(base_dir, filename.split('/')[-2], line_splits[0]))
+        l_pose = []
+        for j in range(1, len(line_splits)):
+            l_pose.append(line_splits[j])
+        np_pose = np.asarray(l_pose)
+        pose[i,:] = np_pose
+    #print(img_list[0])
+    #print(pose[0,:])
+    return(img_list, pose)
+
+def read_images(img_list):
+    imgs = np.zeros((len(img_list), img_rows, img_cols, img_channels))
+    for i in range(0, len(img_list)):
+    #for i in range(0, 1):
+        #print(img_list[i])
+        #loc = dataset_location + img_list[i]
+        #print(loc)
+        im = Image.open(img_list[i])
+        np_im = np.asarray(im, dtype='float32')
+        y,x,ch = np_im.shape
+        c_y = np.floor(y/2)
+        c_x = np.floor(x/2)
+        s_y = int(np.floor(c_y - (img_rows/2)))
+        s_x = int(np.floor(c_x - (img_cols/2)))
+        c_im = np_im[s_y: s_y+img_rows, s_x: s_x+ img_cols, :]
+        #c_im = np.swapaxes(c_im, 1,2)
+        #c_im = np.swapaxes(c_im, 0,1)
+        imgs[i, :, :, :] = c_im
+    return(imgs)
+
+def gather_train_test_txt_list(base_dir):
+    search_path = base_dir + '*/*.txt'
+    txt_list = glob.glob(search_path)
+    train_txt_list = []
+    test_txt_list = []
+    for i in range(0, len(txt_list)):
+        if(txt_list[i].split('_')[-1]=='mod.txt'):
+            #print(os.path.join(base_dir, txt_list[i]))
+            if(txt_list[i].split('_')[-2]=='train'):
+                train_txt_list.append(os.path.join(base_dir, txt_list[i]))
+            elif(txt_list[i].split('_')[-2]=='test'):
+                test_txt_list.append(os.path.join(base_dir, txt_list[i]))
+    print(len(train_txt_list))
+    return (train_txt_list, test_txt_list)
+
+def read_image_and_pose(txt_list):
+    for i in range(0, len(txt_list)):
+        c_img_list, c_pose = process_dataset(txt_list[i])
+        print('Images in current list: ', len(c_img_list))
+        c_imgs = read_images(c_img_list)
+        if(i == 0):
+            pose = c_pose
+            images = c_imgs
+        else:
+            pose = np.concatenate((pose, c_pose), axis=0)
+            images = np.concatenate((images, c_imgs), axis=0)
+    print('total number samples: ', images.shape[0])
+    return(images, pose)
+
+def load_train_test_splits(base_dir):
+    train_txt_list, test_txt_list = gather_train_test_txt_list(base_dir)
+    train_imgs, train_pose = read_image_and_pose(train_txt_list)
+    test_imgs, test_pose = read_image_and_pose(test_txt_list)
+    # Preprocess the data and return the final arrays
+    train_imgs = train_imgs.astype('float32')
+    train_pose = train_pose.astype('float32')
+    train_pose_tx = train_pose[:,:3]
+    train_pose_rt = train_pose[:,3:]
+
+    test_imgs = test_imgs.astype('float32')
+    test_pose = test_pose.astype('float32')
+    test_pose_tx = test_pose[:,:3]
+    test_pose_rt = test_pose[:,3:]
+
+    train_imgs /=255
+    test_imgs /=255
+
+    # make the ds smaller to see if this works
+    '''train_imgs = train_imgs[:100, :, :,:] # 100 images r,c, channels
+    train_pose_tx = train_pose_tx[:100,:] # tx, ty, tz
+    train_pose_rt = train_pose_rt[:100,:] # w, p, q, r quarternion
+    test_imgs = train_imgs[:50, :, : ,:]
+    test_pose_tx = test_pose_tx[:50,:]
+    test_pose_rt= test_pose_rt[:50,:]'''
+
+    return(train_imgs, train_pose_tx, train_pose_rt, test_imgs, test_pose_tx,
+    test_pose_rt)
+
+'''LEGACY function callbacks
 def process_dataset(filename):
     with open(filename) as f:
         lines = f.readlines()
@@ -66,9 +171,10 @@ def read_images(img_list):
         #c_im = np.swapaxes(c_im, 1,2)
         #c_im = np.swapaxes(c_im, 0,1)
         imgs[i, :, :, :] = c_im
-    return(imgs)
+    return(imgs)'''
 
 def main():
+    ''' LEGACY PREPROCESSING
     training_img_list, training_pose = process_dataset(training_file)
     training_pose = training_pose.astype('float32')
     testing_img_list, testing_pose = process_dataset(testing_file)
@@ -83,7 +189,9 @@ def main():
     tr_tx =training_pose[:,:3]
     tr_rt =training_pose[:,3:]
     ts_tx =testing_pose[:,:3]
-    ts_rt =testing_pose[:,3:]
+    ts_rt =testing_pose[:,3:]'''
+
+    #train_imgs, train_pose_tx, train_pose_rt, test_imgs, test_pose_tx, test_pose_rt = load_train_test_splits(base_dir )
 
     # import the resnet model
     base_model = ResNet50(weights='imagenet')
@@ -96,17 +204,24 @@ def main():
     model = Model(input=base_model.input, output=[position, rotation])
     print(model.summary())
 
-    sgd = optimizers.SGD(lr = 0.00001, decay=1e-6, momentum=0.9, nesterov = True)
+    '''sgd = optimizers.SGD(lr = 0.00001, decay=1e-6, momentum=0.9, nesterov = True)
     model.compile(optimizer='SGD', loss='mse', loss_weights =[1.0, 500.0])
 
     tb = TensorBoard(log_dir='./sample_log', histogram_freq=1,
     write_graph=True, write_images=True)
 
-    model.fit(train_imgs, [tr_tx, tr_rt], batch_size = batch_size,
-    epochs= num_epochs, validation_split = 0.1, callbacks= [tb], shuffle=True)
+    model.fit(train_imgs, [train_pose_tx, train_pose_rt], batch_size = batch_size,
+    epochs= num_epochs, callbacks= [tb], shuffle=True)
 
-    ts_pos_pred, ts_rot_pred = model.predict(test_imgs,[ts_tx, tx_tr], verbose=1,
-    batch_size=batch_size)
-    gc.collect()
+    print(test_imgs.shape)
+    print(test_imgs.dtype)
+
+    ts_pos_pred, ts_rot_pred = model.predict(test_imgs, verbose=1)
+
+    print(ts_pos_pred)
+    print('==============================')
+    print(ts_rot_pred)
+    model.save('resnet50_pose.h5')
+    #gc.collect()'''
 if __name__ == '__main__':
     main()
