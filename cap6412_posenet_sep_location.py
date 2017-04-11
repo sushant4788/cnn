e68ef071657a075755710922f584cd937969a8c2
Sushant Kulkarni <sushant4788@gmail.com>
2017-04-11 19:57:50 -0400

    tf device added

--- cap6412_posenet_sep_location.py
+++ cap6412_posenet_sep_location.py
@@ -9,8 +9,8 @@ from keras.callbacks import TensorBoard
 
 use_dummy_ds = False
 use_gpu = True
-batch_size = 1
-num_epochs = 1
+batch_size = 75
+num_epochs = 500
 
 train_splits = [0, 895, 1487, 1220, 3015, 1487, 1532]
 test_splits = [0, 182, 530, 343, 2923, 103, 760]
@@ -46,30 +46,33 @@ def main():
         # Load the current model
         tb_log_dir_name  = './consoliated_logs'
         model_name = 'ds_' + str(k+1) + '.h5'
-        model = cap6412_resnet_inception.inc_pose_net(img_rows, img_cols, img_channels)
-        tb = TensorBoard(log_dir=tb_log_dir_name, histogram_freq=1,
-        write_graph=True, write_images=True)
-        model.fit(c_train_imgs, [c_train_pose_tx, c_train_pose_rt, c_train_pose_tx, c_train_pose_rt,c_train_pose_tx, c_train_pose_rt],
-        batch_size= batch_size, callbacks = [tb], epochs = num_epochs, shuffle=False)
-        model.save(model_name)
-        p_tx_1, p_rx_1, p_tx_2, p_rx_2, p_tx_3, p_rx_3 = model.predict(c_test_imgs)
+        with tf.device('gpu:0'):
+            model = cap6412_resnet_inception.inc_pose_net(img_rows, img_cols, img_channels)
+            tb = TensorBoard(log_dir=tb_log_dir_name, histogram_freq=1,
+            write_graph=True, write_images=True)
+            model.fit(c_train_imgs, [c_train_pose_tx, c_train_pose_rt, c_train_pose_tx, c_train_pose_rt,c_train_pose_tx, c_train_pose_rt],
+            batch_size= batch_size, callbacks = [tb], epochs = num_epochs, shuffle=False)
+            model.save(model_name)
+            p_tx_1, p_rx_1, p_tx_2, p_rx_2, p_tx_3, p_rx_3 = model.predict(c_test_imgs)
 
-        results = np.zeros((c_test_imgs.shape[0], 2), dtype = 'float32')
-        for i in range(0, c_test_imgs.shape[0]):
-            q2 = p_rx_3[i,:] / np.linalg.norm(p_rx_3[i,:])
-            q1 = c_test_pose_rt[i, :] / np.linalg.norm(c_test_pose_rt[i,:])
-            d = abs(np.sum(np.multiply(q1, q2)))
-            theta = 2*np.arccos(d) * 180/math.pi
-            error_x = np.linalg.norm(c_test_pose_tx[i, :] - p_tx_3[i, :])
-            results[i, :] = [error_x, theta]
-            print ('Iteration:  ', i, '  Error XYZ (m):  ', error_x, '  Error Q (degrees):  ', theta)
-        median_result[k,:] = np.median(results,axis=0)
-        print('Median error meters: ', median_result[k,0])
-        print('Median error degrees: ', median_result[k,1])
-        save_dir_name = 'results_' + str(i) + '.txt'
-        np.savetxt(save_dir_name, results, delimiter=' ')
-        print( 'Success!')
-        K.clear_session()
+            results = np.zeros((c_test_imgs.shape[0], 2), dtype = 'float32')
+            for i in range(0, c_test_imgs.shape[0]):
+                q2 = p_rx_3[i,:] / np.linalg.norm(p_rx_3[i,:])
+                q1 = c_test_pose_rt[i, :] / np.linalg.norm(c_test_pose_rt[i,:])
+                d = abs(np.sum(np.multiply(q1, q2)))
+                theta = 2*np.arccos(d) * 180/math.pi
+                error_x = np.linalg.norm(c_test_pose_tx[i, :] - p_tx_3[i, :])
+                results[i, :] = [error_x, theta]
+                print ('Iteration:  ', i, '  Error XYZ (m):  ', error_x, '  Error Q (degrees):  ', theta)
+            median_result[k,:] = np.median(results,axis=0)
+            print('Median error meters: ', median_result[k,0])
+            print('Median error degrees: ', median_result[k,1])
+            save_dir_name = 'results_' + str(i) + '.txt'
+            np.savetxt(save_dir_name, results, delimiter=' ')
+            print( 'Success!')
+            K.clear_session()
+
+
 
 if __name__ == '__main__':
     main()
