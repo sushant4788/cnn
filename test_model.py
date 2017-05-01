cfd576b6f9293ae7f75715794f282c5cb03f5085
Sushant Kulkarni <sushant4788@gmail.com>
2017-05-01 11:42:30 -0400

    Working on the stereo_reg_gru model

--- test_model.py
+++ test_model.py
@@ -3,42 +3,26 @@ from __future__ import print_function
 import numpy as np
 import warnings
 from keras import layers
-from keras.layers import merge, Input
-from keras.layers import Dense, Activation, Flatten, Dropout
-from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D, AveragePooling2D
-from keras.layers import AveragePooling2D
-from keras.layers import BatchNormalization
 from keras.models import Model
-from keras.preprocessing import image
-from keras.utils.layer_utils import convert_all_kernels_in_model
-from keras.utils.data_utils import get_file
-from imagenet_utils import decode_predictions, preprocess_input
 from keras.callbacks import TensorBoard
-from keras import regularizers, optimizers
-from keras.initializers import RandomNormal
-from Local_Resp_Norm import LRN2D
 from keras.models import load_model
-import gc, h5py
-import keras.backend as K
-import posenet_preprocess
-import tensorflow as tf
-import datetime
-
+import gc, h5py, datetime
+from stereo_reg_gru import get_pose_errors, read_KITTI_ds
 
 def main():
-    '''Load the model '''
-    test_file = '/home/sushant/dataset/test.h5'
-    model_file = '/home/sushant/keras_pose_resnet/source/models/posenet_2017-04-05-14-15-37-646073.h5'
+    # Load the dataset
+    dataset_dir = '/home/sushant/kitti_dataset/'
+    train_imgs, train_p_imgs, train_pose_rt, train_pose_tx, test_imgs, test_p_imgs, test_pose_rt, test_pose_tx = read_KITTI_ds(dataset_dir)
+    # Some preporcessing
+    test_imgs -= train_imgs.mean()
+    #Model file name
+    model_file = 'stereo_reg_gru2017-04-30-16-53-00-820403.h5'
+    # Load the model
+    model = load_model(model_file)
+    # Get the prodictions
+    p_tx_1, p_rx_1, p_tx_2, p_rx_2, p_tx_3, p_rx_3 = model.predict()
+    # get the errors
+    get_pose_errors(p_tx_3, p_rx_3, test_pose_tx, test_pose_rt)
 
-    # load the model
-    model = load_model(model_file, custom_objects={'LRN2D':LRN2D})
-    test = h5py.File(ds_dir+test_prefix, 'r')
-    test_imgs = test['test_imgs'][:]
-    test_pose_tx = test['test_pose_tx'][:]
-    test_pose_rt = test['test_pose_rt'][:]
-    test.close()
-
-    p_tx_1, p_rx_1, p_tx_2, p_rx_2, p_tx_3, p_rx_3 = model.predict(test_imgs)
-    print(p_tx_3.shape)
 if __name__ == '__main__':
     main()
